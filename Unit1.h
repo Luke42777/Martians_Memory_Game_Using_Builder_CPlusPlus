cae6287038ae135be9cd1c4d30f85def7e50b035
Lukasz Komsta <lukaszkomsta@yahoo.com>
2020-05-22 13:41:07 +0100

    Interface done, logic remaining

--- Unit1.h
+++ Unit1.h
@@ -7,10 +7,24 @@
 #include <Controls.hpp>
 #include <StdCtrls.hpp>
 #include <Forms.hpp>
+#include <ExtCtrls.hpp>
+#include <Graphics.hpp>
 //---------------------------------------------------------------------------
 class TForm1 : public TForm
 {
 __published:	// IDE-managed Components
+        TImage *background;
+        TLabel *Label1;
+        TImage *b1;
+        TImage *b2;
+        TImage *b3;
+        TImage *b4;
+        TImage *b5;
+        TImage *b1On;
+        TImage *b2On;
+        TImage *b3On;
+        TImage *b4On;
+        TImage *b5On;
 private:	// User declarations
 public:		// User declarations
         __fastcall TForm1(TComponent* Owner);
